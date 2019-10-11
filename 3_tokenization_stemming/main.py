32bd0e47af7622a979f0463d401107cad7974968
Chen Chongsong <chenchongsong@outlook.com>
2019-10-11 22:14:28 +0800

    Task3: tokenization & stemming

---
+++ 3_tokenization_stemming/main.py
@@ -0,0 +1,86 @@
+import nltk
+import json
+import matplotlib.pyplot as plt
+
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+from collections import Counter
+
+STOP_WORDS = stopwords.words("english") + [".", ",", "!", "?", "'s", "...", "(", ")", "n't", "``", "also", "even", "would", "us", "could", "one", "'ve", "-", "''", "'m"]
+print(STOP_WORDS)
+
+
+def stem_text(text):
+    porter = PorterStemmer()
+    tokens = nltk.word_tokenize(text)
+    tokens_stemmed = []
+
+    for token in tokens:
+        tokens_stemmed.append(porter.stem(token))
+    return " ".join(tokens_stemmed)
+
+
+if __name__ == "__main__":
+    texts = []
+    texts_stemmed = []
+    porter = PorterStemmer()
+
+    with open("reviewSelected100.json", encoding="ISO-8859-1") as f:
+        for line in f:
+            review = json.loads(line)
+            texts.append(review["text"])
+            texts_stemmed.append(stem_text(review["text"]))
+
+    len2num = Counter()
+    len2num_stemmed = Counter()
+    word2freq = Counter()
+    word2freq_stemmed = Counter()
+
+    for text in texts:
+        tokens = nltk.word_tokenize(text)
+        len2num[len(tokens)] += 1
+        for token in tokens:
+            unified_token = token.lower()
+            if unified_token not in STOP_WORDS:
+                word2freq[unified_token] += 1
+
+    for text in texts_stemmed:
+        tokens = nltk.word_tokenize(text)
+        len2num_stemmed[len(tokens)] += 1
+        for token in tokens:
+            unified_token = token.lower()
+            if unified_token not in STOP_WORDS:
+                word2freq_stemmed[unified_token] += 1
+
+    lens, nums = zip(*sorted(len2num.items()))
+    lens_stemmed, nums_stemmed = zip(*sorted(len2num_stemmed.items()))
+
+    plt.bar(lens, nums, label="Without Stemming")
+    plt.legend()
+    plt.xlabel("Length of Review")
+    plt.ylabel("No. of Reviews")
+    plt.title("Distribution of Review Lengths (without Stemming)")
+    plt.savefig("3_tokenization_stemming/fig3-1-dist-nostem")
+    # plt.show()
+    plt.clf()
+
+    plt.bar(lens_stemmed, nums_stemmed, label="With Stemming")
+    plt.legend()
+    plt.xlabel("Length of Review")
+    plt.ylabel("No. of Reviews")
+    plt.title("Distribution of Review Lengths (without Stemming)")
+    plt.savefig("3_tokenization_stemming/fig3-2-dist-withstem")
+    # plt.show()
+
+    print("Without Stemming")
+    print("\tMax Review Length: {}".format(max(lens)))  # 1194
+    print("\tLength {} has a maximum number of reviews: {}".format(nums.index(max(nums)), max(nums)))  # 30, 132
+    print("\tTop 20 frequent words: {}".format(word2freq.most_common(20)))
+
+    print("With Stemming")
+    print("\tMax Review Length: {}".format(max(lens_stemmed)))  # 1194
+    print("\tLength {} has a maximum number of reviews: {}".format(nums_stemmed.index(max(nums_stemmed)), max(nums_stemmed)))
+    # 30, 133 (slightly different from 132 due to syntax error of reviews e.g. " ., ")
+    print("\tTop 20 frequent words: {}".format(word2freq_stemmed.most_common(20)))
+
+
