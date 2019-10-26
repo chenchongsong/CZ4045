4ff1c0818b29f4a099f7c5ec678e3a2f01d48b3d
Chen Chongsong <chenchongsong@outlook.com>
2019-10-26 16:21:04 +0800

    Finalize task6

--- 6_summarizer.py
+++ 6_summarizer.py
@@ -1,60 +1,111 @@
 import spacy
+from collections import Counter
+from common import get_five_random_businesses
 
 nlp = spacy.load("en_core_web_sm")
 
-sample_doc1 = nlp("""
-    I ordered the sea bass special. It was to die for. Cooked perfectly, seasoned perfectly, perfect portion.
-    I can not say enough good things about this dish.
-    When the server asked how it was he seemed very proud of the dish and said, \" doesn't she (the chef) do an incredible job?\"
-    She does. My hubby got the crab tortellini and also loved his.
-    I heard \"mmmm this is so good\" from all around the table.
-    Our waiter was super nice and even gave us free desserts because we were some of the last people in the restaurant.
-    Service was very slow and the place was PACKED but we had our jugs of wine and a large group with good conversation so it didn't seem to bother anyone.
-    So-Do order the calamari and fried zucchini appetizers. Leave out the mussels.
-    If they have the sea bass special, I highly recommend it.
-    The chicken parm and crab tortellini were also very good and very big.
-    The chicken Romano was a bit bland. The house salads were teeny.
-    Do make a reservation but still expect to wait for your food.
-    Go with a large group of people and plan for it to be loud.
-    Don't go with a date unless you're fighting and don't feel like hearing anything they have to say. 
-    Ask to sit in the side room if it's available.""")
+
+def concat_noun_with_compound(noun):
+    for child in noun.children:
+        if child.pos_ in ["NOUN", "PROPN"] and child.dep_ == "compond":
+            return child.text + " " + noun.text
+    return noun.text
+
+
+def concat_adj_with_adv(adj):
+    for child in adj.children:
+        if child.pos_ == "ADV" and child.dep_ == "advmod":
+            return child.text + " " + adj.text
+    return adj.text
 
 
 def get_noun_adj_pairs(doc):
     result_pairs = []
     for token in doc:
-        if token.pos_ == "ADJ" and token.dep_ == "amod":
-            if token.head.pos_ == "NOUN":
-                result_pairs.append((token.head.text, token.text))
 
+        # for adjectives as "amod" (adjectival modifier)
+        # e.g. a red and fresh apple
+        if token.pos_ == "ADJ" and token.dep_ == "amod":
+            if token.head.pos_ in ["NOUN", "PROPN"]:
+                noun_list = [token.head]
+                adj_list = [token]
+                for child in token.children:
+                    if child.pos_ == "ADJ" and child.dep_ == "conj":
+                        adj_list.append(child)
+
+                for noun in noun_list:
+                    for adj in adj_list:
+                        noun_string = concat_noun_with_compound(noun)
+                        adj_string = concat_adj_with_adv(adj)
+                        result_pairs.append((noun_string, adj_string))
+
+        # for adjectives as "acomp" (adjectival complement)
+        # e.g. The apple is red and fresh.
         if token.pos_ in ["NOUN", "PROPN"] and token.dep_ == "nsubj":
-            noun_list = [token]
-            for noun in noun_list:
-                for child in noun.children:
-                    if child.pos_ in ["NOUN", "PROPN"] and child.dep_ == "conj" and child not in noun_list:
-                        noun_list.append(child)
+            noun = token
+            noun_list = [noun]
+
+            for child in noun.children:
+                if child.pos_ in ["NOUN", "PROPN"] and child.dep_ == "conj" and child not in noun_list:
+                    noun_list.append(child)
 
             adj_list = []
-            for r in token.head.rights:
+            for r in noun.head.rights:  # find the first adjective after the noun
                 if r.pos_ == "ADJ":
                     adj_list.append(r)
                     break
-            for adj in adj_list:
-                for child in adj.children:
-                    if child.pos_ == "ADJ" and child.dep_ == "conj" and child not in adj_list:
-                        adj_list.append(child)
-                        break
+            if len(adj_list) == 0:
+                continue
+            adj = adj_list[0]
+            for child in adj.children:
+                if child.pos_ == "ADJ" and child.dep_ == "conj" and child not in adj_list:
+                    adj_list.append(child)
 
             for noun in noun_list:
                 for adj in adj_list:
-                    result_pairs.append((noun.text, adj.text))
+                    noun_string = concat_noun_with_compound(noun)
+                    adj_string = concat_adj_with_adv(adj)
+                    result_pairs.append((noun_string, adj_string))
 
     return result_pairs
 
 
 if __name__ == "__main__":
-    doc_demo = nlp("The chicken parm, and crab tortellini were also very good and very big.")
-    for token in doc_demo:
-        print(token.text, token.dep_, token.head.text, token.head.pos_, [child for child in token.children], token.pos_, token.tag_)
-    print(get_noun_adj_pairs(doc_demo))
-    print(get_noun_adj_pairs(sample_doc1))
+    print("Top 5 common <noun, adj> pairs for each of the 5 businesses:")
+    businesses = get_five_random_businesses()
+    for business_id, texts in businesses.items():
+        assert len(texts) == 100
+        counter = Counter()
+
+        for text in texts:
+            doc = nlp(text)
+            pairs = get_noun_adj_pairs(doc)
+            for pair in pairs:
+                noun, adj = pair
+                counter[(noun.lower(), adj.lower())] += 1
+
+        top5_pairs = counter.most_common(5)
+        print(top5_pairs)
+
+    print("\nDemo sentences:")
+    text_demo_1 = "The crab and chicken wings were also great and extremely big."
+    doc_demo_1 = nlp(text_demo_1)
+    # crab     <-------------- were
+    # wings    <-----conj----- crab
+    # chicken  <---compound--- wings
+    #
+    # great    <-------------- were
+    # big      <-----conj----- great
+    # extremely<----advmod---- big
+
+    print(text_demo_1)
+    print(get_noun_adj_pairs(doc_demo_1))
+    print("\n")
+
+    text_demo_2 = "A popular and nice dish!"
+    doc_demo_2 = nlp(text_demo_2)
+    # popular <---amod--- dish
+    # nice    <---conj--- popular
+    print(text_demo_2)
+    print(get_noun_adj_pairs(doc_demo_2))
+
--- common.py
+++ common.py
@@ -23,6 +23,18 @@ def get_five_random_texts():
     return texts
 
 
+def get_five_random_businesses():
+    selected_business_ids = ["ZBE-H_aUlicix_9vUGQPIQ", "e-YnECeZNt8ngm0tu4X9mQ", "j7HO1YeMQGYo3KibMXZ5vg", "7e3PZzUpG5FYOTGt3O3ePA", "vuHzLZ7nAeT-EiecOkS5Og"]
+    businesses = dict([(business_id, []) for business_id in selected_business_ids])
+    with open("reviewSelected100.json", encoding="ISO-8859-1") as f:
+        for line in f:
+            review = json.loads(line)
+            business_id = review["business_id"]
+            if business_id in selected_business_ids:
+                businesses[business_id].append(review["text"])
+    return businesses
+
+
 if __name__ == "__main__":
     print("First randomly selected 5 reviews\n")
     texts = get_five_random_texts()
