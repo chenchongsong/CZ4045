c2241a1550d3a593a2f5a2f4d0da82699a123223
cz4045common <56721176+cz4045common@users.noreply.github.com>
2019-10-30 19:55:09 +0800

    Update 4_pos_tagging.py

--- 4_pos_tagging.py
+++ 4_pos_tagging.py
@@ -14,5 +14,10 @@ def pos_tagging(sample):
 
 
 if __name__ == '__main__':
-    sample = ["", "", "", "", ""]  # TODO put 5 sample sentences from each of the 5 reviews
+    sample1 = "I love love their Kalbi, I always order it the sauce is what makes it really good."
+    sample2 = "I reread the description of the burgetta burger and realized that I didn't even get the fried eggplant!"
+    sample3 = "Good sushi, fast service!"
+    sample4 = "With all the food groups covered, we had a really enjoyable time. "
+    sample5 = "But he did ask me if I knew how to turn the temp knob. "
+    sample = [sample1, sample2, sample3, sample4, sample5]
     pos_tagging(sample)
